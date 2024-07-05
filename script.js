2421bdb3948dcb104b411c3c3af4f8e47721db6a
Ravisharma-1999 <ravi829144@gmail.com>
2024-07-05 21:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+() => {
+  // Check left and right arrows have carousel-control class
+  cy.get(".left").should("have.class", "carousel-control");
+  cy.get(".right").should("have.class", "carousel-control");
+
+  // Check for the glyphicon classes
+  cy.get(".glyphicon-chevron-right").should("exist");
+  cy.get(".glyphicon-chevron-left").should("exist");
+  cy.get(".glyphicon").should("exist");
+}
